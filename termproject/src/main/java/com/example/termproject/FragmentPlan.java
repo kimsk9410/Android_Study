1fc7ab4b0ae6acd3dd00a6e72948d7014c64cefa
kimsk9410 <lsn9410@naver.com>
2016-11-28 16:44:08 +0900

    Walk Layout

--- termproject/src/main/java/com/example/termproject/FragmentPlan.java
+++ termproject/src/main/java/com/example/termproject/FragmentPlan.java
@@ -1,7 +1,9 @@
 package com.example.termproject;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,10 @@ import android.view.ViewGroup;
  */
 
 public class FragmentPlan extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.plan, container, false);
